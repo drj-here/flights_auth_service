87e8d8799113640153c7221b25629e10a884f4c7
Dishu <dishuraj129@gmail.com>
2024-02-29 11:12:18 +0530

    added jwt create and verify logic

--- src/service/user-service.js
+++ src/service/user-service.js
@@ -1,5 +1,6 @@
 const UserRepository=require('../repository/user-repo')
-
+const jwt=require('jsonwebtoken')
+const {JWT_KEY}=require('../config/serverConfig')
 class UserService{
     constructor(){
         this.userRepository=new UserRepository()
@@ -23,6 +24,28 @@ class UserService{
         }
         catch(error){
             console.log("Something went wrong in the service layer")
+            throw error;
+        }
+    }
+
+    createToken(user){
+        try{
+           const token=jwt.sign(user,JWT_KEY,{expiresIn:'1d'})
+           return token
+        }
+        catch(error){
+            console.log("Something went in token creation")
+            throw error;
+        }
+    }
+
+    verifyToken(token){
+        try{
+           const response=jwt.verify(token,JWT_KEY)
+           return response
+        }
+        catch(error){
+            console.log("Something went wrong in token validation")
             throw error;
         }
     }
